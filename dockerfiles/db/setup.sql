912015fa9a16e0b6c7de15e1c082414818dca97e
Yunus Emre Kepenek <yunus.kepenek@radity.com>
2024-09-01 13:14:07 +0300

    Implement database schema and seed database

--- dockerfiles/db/setup.sql
+++ dockerfiles/db/setup.sql
@@ -32,3 +32,75 @@ as $$
         extensions := extensions
     );
 $$;
+
+CREATE TABLE public.users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE public.lessons (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title TEXT,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE public.versions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    lesson_id UUID NOT NULL,
+    version_number INT,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (lesson_id) REFERENCES public.lessons(id)
+);
+
+CREATE TABLE public.storyboards (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    lesson_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    title TEXT NOT NULL,
+    content TEXT,
+    position INT,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (lesson_id) REFERENCES public.lessons(id),
+    FOREIGN KEY (user_id) REFERENCES public.users(id)
+);
+
+CREATE TABLE public.files (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    storyboard_id UUID NOT NULL,
+    name TEXT,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (storyboard_id) REFERENCES public.storyboards(id)
+);
+
+CREATE TABLE public.comments (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    storyboard_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    content TEXT,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (storyboard_id) REFERENCES public.storyboards(id),
+    FOREIGN KEY (user_id) REFERENCES public.users(id)
+);
+
+insert into public.users(email, username, password, created_at)
+values
+    ('yunus@st.com', 'yunusemre', 'topsecretpass', now()),
+    ('emre@st.com', 'emre', 'indestructiblepass', now());
+
+insert into public.lessons(title, created_at)
+values
+    ('Welcome', now()),
+    ('Lesson 2', now()),
+    ('Lesson 3', now());
+
+insert into public.versions(lesson_id, version_number, created_at)
+values
+    ((SELECT id FROM public.lessons WHERE title = 'Welcome'), 1, now()),
+    ((SELECT id FROM public.lessons WHERE title = 'Welcome'), 2, now()),
+    ((SELECT id FROM public.lessons WHERE title = 'Lesson 2'), 1, now()),
+    ((SELECT id FROM public.lessons WHERE title = 'Lesson 3'), 1, now());
+
+comment on schema public is '@graphql({"inflect_names": true})';
